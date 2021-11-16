349568b39102b2eecaf89d745f6193ad3e9dc415
yeongleej <xzba13@naver.com>
2021-11-17 02:37:32 +0900

    Board 모델 생성

--- board/models.py
+++ board/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import *
 
 # Create your models here.
+
+class Board(models.Model):
+    user_nickname = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user_nickname")
+    title = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title    
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_nickname = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user_nickname")
+    cotent = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
