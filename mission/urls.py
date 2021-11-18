5ce493c858d103d6a9a2d206ea19c78dd40ffdc1
yeongleej <xzba13@naver.com>
2021-11-19 03:47:10 +0900

    [지영] 미션 템플릿 수정&미션 뷰 수정&템플릿,뷰 연결완료

--- mission/urls.py
+++ mission/urls.py
@@ -6,5 +6,4 @@ app_name = 'mission'
 urlpatterns = [
     path('mission_list/', mList_view, name='mission_list'),
     path('add_mission/', add_mission_view, name='add_mission'),
-    path('perform_mission/', perform_mission_view, name='perform_mission'),
 ]
