848673e3aa1b269231a6e1f10227eb3cdf3d7d03
yeongleej <xzba13@naver.com>
2021-11-20 02:03:41 +0900

    [지영] 게시판 완료

--- board/urls.py
+++ board/urls.py
@@ -5,6 +5,7 @@ from .views import *
 app_name = 'board'
 urlpatterns = [
     path('question/', question_view, name='question'),
-    path('question_detail', question_detail_view, name='question_detail'),
+    path('question_detail/<int:id>', question_detail_view, name='question_detail'),
+    path('make_comment/', make_comment_view, name='make_comment'),
     path('make_question/', make_question_view, name='make_question'),
 ]
