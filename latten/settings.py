dd4977330c59e5df91c59eb0a2d60df87dd9a33b
yeongleej <xzba13@naver.com>
2021-11-20 06:29:07 +0900

    [지영] settings

--- latten/settings.py
+++ latten/settings.py
@@ -124,8 +124,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'board'),
+    os.path.join(BASE_DIR, 'mission'),
+    os.path.join(BASE_DIR, 'users'),
+    os.path.join(BASE_DIR, 'main'),
+
+
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
