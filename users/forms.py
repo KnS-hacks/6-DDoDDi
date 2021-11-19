ba1b1ad06e2dc820e3ff6d1d1afae84af12fe096
yulhee741 <dbfgml980413@gmail.com>
2021-11-20 03:13:15 +0900

    [율희] feat: 쪽지함 디테일 연결

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class LetterForm(forms.ModelForm):
     class Meta:
         model = Letter
-        fields = ['recipient', 'title', 'comment']
+        fields = ['recipient', 'title', 'comment', 'sender']
         labels = {
             'recipient':_('받는이'),
             'title':_('쪽지 제목'),
